2ba18ef9a43e0aa6bcbc95fbc68a31a146c79b51
Wagner <wagner.rodrigues@dbccompany.com.br>
2022-08-18 00:58:30 -0300

    Criação dos elementos Row e Box

--- src/components/Fuel/index.tsx
+++ src/components/Fuel/index.tsx
@@ -1,10 +1,17 @@
-import { Container, SettingsIcon, Title } from "./styles";
+import { Box, Container, Panel, Row, SettingsIcon, Title } from "./styles";
 
 export const FuelComponent = () => {
   return (
     <Container>
       <Title>Posto ReactJS</Title>
       <SettingsIcon />
+
+      <Panel>
+        <Row>
+          <Box>Teste 1</Box>
+          <Box>Teste 2</Box>
+        </Row>
+      </Panel>
     </Container>
   );
 };
